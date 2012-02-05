4ee8343400a99b15348c5d40f18f1de019eae0bf
Sebastien Delaire <pes@gandalf.(none)>
2012-02-05 15:44:07 +0100

    initial project structure

---
+++ src/org/copains/ludroid/LudroidMainActivity.java
@@ -0,0 +1,13 @@
+package org.copains.ludroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LudroidMainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
