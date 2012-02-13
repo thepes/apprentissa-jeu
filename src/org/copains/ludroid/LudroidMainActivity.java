998d4b91627330352f587ea493119cb037595e1f
Sebastien Delaire <the.pes@gmail.com>
2012-02-14 00:36:44 +0100

    first working test of TTS

--- src/org/copains/ludroid/LudroidMainActivity.java
+++ src/org/copains/ludroid/LudroidMainActivity.java
@@ -1,31 +1,49 @@
 package org.copains.ludroid;
 
+import java.util.Locale;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
 import android.util.Log;
 
-public class LudroidMainActivity extends Activity {
+public class LudroidMainActivity extends Activity implements OnInitListener {
 	
-	private static final int CHECK_TTS_DATA	= 0;
+	private static final int CHECK_TTS_DATA	= 100;
+	
+	private TextToSpeech ttsEngine = null;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        init();
         setContentView(R.layout.main);
+        //init();
+        ttsEngine = new TextToSpeech(this,this);
+		
     }
     
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
     	if (requestCode == CHECK_TTS_DATA) {
     		Log.i("Ludroid","resultCode = " + resultCode);
+    		String[] voiceInfo = data.getStringArrayExtra(TextToSpeech.Engine.EXTRA_VOICE_DATA_FILES_INFO);
+    		if (null != voiceInfo) {
+    			for (int i = 0 ; i < voiceInfo.length ; i ++) {
+    				Log.d("ludroid","Voice INFO : " + voiceInfo[i]);
+    				
+    			}
+    		}
     		switch (resultCode) {
     			case TextToSpeech.Engine.CHECK_VOICE_DATA_MISSING_DATA:
     				Intent installTts = new Intent(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
     				startActivity(installTts);
+    				break;
+    			case TextToSpeech.Engine.CHECK_VOICE_DATA_PASS:
+    				Log.i("ludroid","TTS data OK");
+    				ttsEngine = new TextToSpeech(this,this);
     				break;
 				default:
 					Log.i("ludroid","unhandled code : " + requestCode);
@@ -41,5 +59,12 @@ public class LudroidMainActivity extends Activity {
     	Intent checkIntent = new Intent(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
     	startActivityForResult(checkIntent, CHECK_TTS_DATA);
     }
+
+	@Override
+	public void onInit(int arg0) {
+		// TODO Auto-generated method stub
+		ttsEngine.setLanguage(Locale.FRENCH);
+		ttsEngine.speak("Bonjour amour de ma vie ! Je t'aime de tout mon coeur !", TextToSpeech.QUEUE_FLUSH, null);
+	}
     
 }
