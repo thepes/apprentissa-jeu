658c21a9234b1dcae2458db5b6f102f31370a669
Sebastien Delaire <the.pes@gmail.com>
2012-03-18 17:23:30 +0100

    - work on account creation form

---
+++ src/org/copains/ludroid/account/activities/AccountCreationActivity.java
@@ -0,0 +1,17 @@
+package org.copains.ludroid.account.activities;
+
+import org.copains.ludroid.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AccountCreationActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.account_creation);
+	}
+
+}
