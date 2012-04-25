b0bfb96fb141a6d2ea749431d62a55aa54b0ebef
Sebastien Delaire <the.pes@gmail.com>
2012-04-26 00:42:02 +0200

    - work on find colors (Issue 9)

--- src/org/copains/ludroid/games/findcolors/controller/FindColorsMg.java
+++ src/org/copains/ludroid/games/findcolors/controller/FindColorsMg.java
@@ -1,22 +1,46 @@
 package org.copains.ludroid.games.findcolors.controller;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Random;
 
+import org.copains.ludroid.R;
+
+import android.graphics.Color;
 import android.graphics.Rect;
 
 public class FindColorsMg {
 
 	private static FindColorsMg instance = null;
-	private Hashtable<String,Rect> placedSquares = null;
+	private Hashtable<Integer,Rect> placedSquares = null;
 	private int sizeX, sizeY, squareSize;
 	private Random rnd;
+	private List<Integer> colors;
+	private Hashtable<Integer, Integer> colorCodes;
+	private boolean gameStarted;
+	private boolean gameFinished;
+	private int colorToFind;
 	
 	private FindColorsMg() {
 		rnd = new Random();
-		placedSquares = new Hashtable<String, Rect>();
+		placedSquares = new Hashtable<Integer, Rect>();
+		colors = new ArrayList<Integer>();
+		colorCodes = new Hashtable<Integer, Integer>();
+		colors.add(R.string.findColors_blue);
+		colorCodes.put(R.string.findColors_blue,Color.argb(255, 0, 0, 255));
+		colors.add(R.string.findColors_green);
+		colorCodes.put(R.string.findColors_green,Color.argb(255, 0, 255, 0));
+		colors.add(R.string.findColors_red);
+		colorCodes.put(R.string.findColors_red,Color.argb(255, 255, 0, 0));
+		colors.add(R.string.findColors_orange);
+		colorCodes.put(R.string.findColors_orange,Color.argb(255, 255, 180, 48));
+		colors.add(R.string.findColors_pink);
+		colorCodes.put(R.string.findColors_pink,Color.argb(255, 253, 162, 255));
+		colors.add(R.string.findColors_yellow);
+		colorCodes.put(R.string.findColors_yellow,Color.argb(255, 252, 255, 19));
 	}
 	
 	public static FindColorsMg getInstance() {
@@ -26,6 +50,20 @@ public class FindColorsMg {
 		return (instance);
 	}
 	
+	public void initGame(int sizeX, int sizeY) {
+		this.sizeX = sizeX;
+		this.sizeY = sizeY;
+		squareSize = Math.min(sizeX, sizeY) / 4;
+		placedSquares = new Hashtable<Integer, Rect>();
+		List<Integer> colors = Collections.list(colorCodes.keys());
+		for (Integer col : colors) {
+			placedSquares.put(col, getUnusedRandomPosition());
+		}
+		gameStarted = true;
+		gameFinished = false;
+		colorToFind = rnd.nextInt(colorCodes.size());
+	}
+
 	public boolean checkClick(String color, int x, int y) {
 		Rect r = placedSquares.get(color);
 		return (r.contains(x, y));
