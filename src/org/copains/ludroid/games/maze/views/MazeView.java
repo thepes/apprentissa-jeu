41076992e09370f9d567ec93c24f72523ec4c0b7
Sebastien Delaire <the.pes@gmail.com>
2012-06-19 23:02:13 +0200

    - bugfix and code cleaning

--- src/org/copains/ludroid/games/maze/views/MazeView.java
+++ src/org/copains/ludroid/games/maze/views/MazeView.java
@@ -56,10 +56,9 @@ public class MazeView extends View implements OnInitListener {
 		MazeMg mg = MazeMg.getInstance();
 		if (!mg.isGameStarted()) {
 			mg.initGame(cellsX, cellsY);
-			//initMazeBackground(canvas);
 		}
-		stepX = canvas.getWidth()/cellsX;
-		stepY = canvas.getHeight()/cellsY;
+		stepX = this.getMeasuredWidth()/cellsX;
+		stepY = this.getMeasuredHeight()/cellsY;
 		Maze maze = mg.getMaze();
 		Paint p = new Paint();
 		p.setARGB(255, 255, 255, 255);
@@ -99,36 +98,14 @@ public class MazeView extends View implements OnInitListener {
 		y = mg.getCurrentPlayerPosition().getY();
 		r = new Rect(x*stepX, y*stepY, (x+1)*stepX, (y+1)*stepY);
 		canvas.drawRect(r, p);
-		//drawnMaze.draw(canvas);
-	}
-
-	private void initMazeBackground(Canvas canvas) {
-		drawnMaze = new Picture();
-		Canvas c = drawnMaze.beginRecording(canvas.getWidth(), canvas.getHeight());
-		int stepX, stepY;
-		stepX = canvas.getWidth()/cellsX;
-		stepY = canvas.getHeight()/cellsY;
-		MazeMg mg = MazeMg.getInstance();
-		Maze maze = mg.getMaze();
-		Paint p = new Paint();
-		p.setARGB(255, 255, 255, 255);
-		p.setStyle(Style.FILL);
-		byte[][] cells = maze.getMaze();
-		for (int j = 0 ; j < cellsY ; j++)
-			for (int i = 0 ; i < cellsX ; i++) {
-				if (cells[i][j] == 1) {
-					Rect r = new Rect(i*stepX, j*stepY, (i+1)*stepX, (j+1)*stepY);
-					c.drawRect(r, p);
-				}
-			}
-		drawnMaze.endRecording();
 	}
 	
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		gestureDetector.onTouchEvent(event);
+		
+		// when the user release the screen, we reinit all scroll stuff
 		if ((event.getActionMasked() & MotionEvent.ACTION_UP) == MotionEvent.ACTION_UP) {
-			Log.i("ludroid","action UP");
 			MazeMg mg = MazeMg.getInstance();
 			mg.reinitPreviousPosition();
 			crossedWall = false;
@@ -142,26 +119,19 @@ public class MazeView extends View implements OnInitListener {
 		public boolean onScroll(android.view.MotionEvent sourceEvt, android.view.MotionEvent destEvt, float distanceX, float distanceY) {
 			MazeMg mg = MazeMg.getInstance();
 			
-			//Log.i("ludroid", "source pos : " + sourceEvt.getX() + " / " + sourceEvt.getY());
-			//Log.i("ludroid", "dest pos : " + destEvt.getX() + " / " + destEvt.getY());
-			//Log.i("ludroid","distanceY = " + distanceY);
 			if (crossedWall)
 				return false;
 			Coordinates sourceCell = new Coordinates((int)sourceEvt.getX(), (int)sourceEvt.getY());
 			Coordinates convSrc = CoordinatesConverter.convert(sourceCell, stepX, stepY);
 			if (mg.getCurrentPlayerPosition().equals(convSrc)
 					|| mg.getPreviousPlayerPosition().equals(convSrc)) {
-				Log.i("ludroid", "Ok pour le départ");
 				Coordinates destCell = CoordinatesConverter.convert(new Coordinates((int)destEvt.getX(), (int)destEvt.getY()), stepX, stepY);
 				if (!destCell.equals(sourceCell)) {
-					Log.i("ludroid","Changement de case");
 					if (mg.isWall(destCell)) {
-						Log.i("ludroid"," droit dans le mur");
 						mg.reinitPreviousPosition();
 						crossedWall = true;
 						return true;
 					} else {
-						Log.i("ludroid","c'est bon !");
 						mg.handleMove(destCell);
 						invalidate();
 						if (destCell.equals(mg.getMaze().getEndPoint())) {
